74dde0ee07b86c7768444753410ac40c05fdeef2
Darren Hoyland <darren.hoyland@canonical.com>
2015-07-27 10:13:15 +0100

    Having webdriver issues with the lastest version of Firefox - will remove the functional tests (which are barely started anyway) until we can secify a version to run the tests on

--- weebl/tests_functional.py
+++ weebl/tests_functional.py
@@ -33,51 +33,7 @@ class SettingsTests(WebDriverOilTestCase):
 
     def tearDown(self):
         self.typey('#id_check_in_unstable_threshold', randint())
-
         super(SettingsTests, self).tearDown()
-
-    def test_deploy_stats(self):
-        print('wtf?')
-        self.go_to_url("{}/Settings/".format(self.HOST))
-        #import pdb; pdb.set_trace()
-
-'''
-class CommonTests(WebDriverOilTestCase):
-
-    def test_deploy_stats(self):
-        self.driver.get("{}/".format(self.HOST))
-        import pdb; pdb.set_trace()
-
-    def test_prepare_stats(self):
-        self.driver.get("{}/".format(self.HOST))
-        import pdb; pdb.set_trace()
-    def test_tempest_stats(self):
-        self.driver.get("{}/".format(self.HOST))
-        import pdb; pdb.set_trace()
-
-    def test_state(self):
-        self.driver.get("{}/".format(self.HOST))
-        import pdb; pdb.set_trace()
-
-    def test_success(self):
-        self.driver.get("{}/".format(self.HOST))
-        import pdb; pdb.set_trace()
-
-
-class MainPageTests(WebDriverOilTestCase):
-
-    def test_breakdown(self):
-        self.driver.get("{}/".format(self.HOST))
-        import pdb; pdb.set_trace()
-
-
-class JobSpecificBugsListPageTests(WebDriverOilTestCase):
-
-    def test_job_specific_bugs_list(self):
-        page_name = 'job_specific_bugs_list'
-        self.driver.get("{}/job/pipeline_deploy".format(self.HOST))
-        assert page_name in self.driver.title
-'''
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weebl.settings")
