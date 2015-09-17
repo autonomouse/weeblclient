2e9e6061b61a31a51c26d72e978b3779b05692dd
Jason Hobbs <jason.hobbs@canonical.com>
2015-09-17 16:04:50 -0500

    First working java script.

---
+++ weebl/oilserver/static/js/weebl.js
@@ -0,0 +1,23 @@
+angular.module('weebl', ['oil_builds'])
+.controller('weeblController', ['buildsRetriever', function(buildsRetriever) {
+   buildsRetriever.refresh(this);
+//   this.count = buildsRetriever.total_count;
+}]);
+
+angular.module('oil_builds', [])
+.factory('buildsRetriever', ['$http', function($http) {
+  var build_count = -1;
+  var refresh = function(scope) {
+    var url = "/api/v1/build/";
+    return $http.get(url).success(function(data) {
+       scope.count = data.meta.total_count;
+    });
+  };
+
+  // refresh();
+
+  return {
+    total_count: build_count,
+    refresh: refresh
+  };
+}]);
