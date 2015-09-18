56e5fb1ce04a1fd1740ab77db3eb7bc55ed7272b
Darren Hoyland <darren.hoyland@canonical.com>
2015-09-18 10:29:45 -0400

    Separate controllers in separate files

--- weebl/oilserver/static/js/bugs.js
+++ weebl/oilserver/static/js/bugs.js
@@ -1,10 +1,5 @@
 var app = angular.module('stats', []);
 
-app.controller('buildsController', ['buildsRetriever', function(buildsRetriever) {
-    buildsRetriever.refresh(this);
- //   this.count = buildsRetriever.total_count;
- }]);
-
 app.controller('bugsController', ['bugsRetriever', function(bugsRetriever) {
     bugsRetriever.refresh(this);
  //   this.count = bugsRetriever.total_count;
--- weebl/oilserver/static/js/bugs_retriever.js
+++ weebl/oilserver/static/js/bugs_retriever.js
@@ -1,6 +1,6 @@
-app.factory('buildsRetriever', ['$http', function($http) {
+app.factory('bugsRetriever', ['$http', function($http) {
   var refresh = function(scope) {
-    var url = "/api/v1/build/";
+    var url = "/api/v1/bug/";
     return $http.get(url).success(function(data) {
        scope.count = data.meta.total_count;
     });
