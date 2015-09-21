5e7f7a0ce2716ef1e1dd9df5536eaf2e63bf05e8
Jason Hobbs <jason.hobbs@canonical.com>
2015-09-21 18:46:02 -0500

    Add pipeline prepare and cloud image count numbers.

--- weebl/oilserver/static/js/weebl.js
+++ weebl/oilserver/static/js/weebl.js
@@ -5,6 +5,8 @@ app.controller('buildsController', ['buildsRetriever', function(buildsRetriever)
     finish_date = '2016-01-01';
     buildsRetriever.refresh(this, 'pipeline_count', 'pipeline_deploy', start_date, finish_date);
     buildsRetriever.refresh(this, 'pass_deploy_count', 'pipeline_deploy', start_date, finish_date, 'success');
+    buildsRetriever.refresh(this, 'pass_prepare_count', 'pipeline_prepare', start_date, finish_date, 'success');
+    buildsRetriever.refresh(this, 'pass_test_cloud_image_count', 'test_cloud_image', start_date, finish_date, 'success');
  }]);
 
 app.controller('bugsController', ['bugsRetriever', function(bugsRetriever) {
