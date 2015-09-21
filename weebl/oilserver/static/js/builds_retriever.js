9f851ab4205c08ac76c87deb77ebb16fdcb39984
Jason Hobbs <jason.hobbs@canonical.com>
2015-09-21 17:40:22 -0500

    Show the number of pipelines, not builds.

--- weebl/oilserver/static/js/builds_retriever.js
+++ weebl/oilserver/static/js/builds_retriever.js
@@ -1,7 +1,15 @@
 app.factory('buildsRetriever', ['$http', function($http) {
-  var refresh = function(scope) {
+  var refresh = function(scope, job_type_name, created_at_start, created_at_end) {
     var url = "/api/v1/build/";
-    return $http.get(url).success(function(data) {
+    var parameters = [];
+    if (job_type_name)
+        parameters['job_type__name'] = job_type_name;
+    if (created_at_start)
+        parameters['created_at__gte'] = created_at_start;
+    if (created_at_end)
+        parameters['created_at__lte'] = created_at_end;
+
+    return $http.get(url, {'params': parameters}).success(function(data) {
        scope.count = data.meta.total_count;
     });
   };
--- weebl/oilserver/static/js/weebl.js
+++ weebl/oilserver/static/js/weebl.js
@@ -1,7 +1,7 @@
 var app = angular.module('stats', []);
 
 app.controller('buildsController', ['buildsRetriever', function(buildsRetriever) {
-    buildsRetriever.refresh(this);
+    buildsRetriever.refresh(this, 'pipeline_deploy', '2015-01-01', '2016-01-01');
  }]);
 
 app.controller('bugsController', ['bugsRetriever', function(bugsRetriever) {
