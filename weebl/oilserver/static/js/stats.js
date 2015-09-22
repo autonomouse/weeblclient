6c88a016dfbe8b6fd0099503612ac8d6dbacec3c
Jason Hobbs <jason.hobbs@canonical.com>
2015-09-22 17:07:17 -0500

    Get some structure in place for updating by date.

--- weebl/oilserver/static/js/stats.js
+++ weebl/oilserver/static/js/stats.js
@@ -8,14 +8,46 @@ $interpolateProvider.endSymbol('$}');
 builds_app.controller('buildsController', [
     '$scope', 'buildsRetriever',
     function($scope, buildsRetriever) {
-        start_date = '2015-01-01';
-        finish_date = '2016-01-01';
-        buildsRetriever.refresh(this, 'pipeline_count', 'pipeline_deploy', start_date, finish_date);
-        buildsRetriever.refresh(this, 'pass_deploy_count', 'pipeline_deploy', start_date, finish_date, 'success');
-        buildsRetriever.refresh(this, 'pass_prepare_count', 'pipeline_prepare', start_date, finish_date, 'success');
-        buildsRetriever.refresh(this, 'pass_test_cloud_image_count', 'test_cloud_image', start_date, finish_date, 'success');
+        binding = this;
+
+        function updateStats(start_date, finish_date) {
+            buildsRetriever.refresh(binding, 'pipeline_count',
+                                    'pipeline_deploy', start_date,
+                                    finish_date);
+            buildsRetriever.refresh(binding, 'pass_deploy_count',
+                                    'pipeline_deploy', start_date,
+                                    finish_date, 'success');
+            buildsRetriever.refresh(binding, 'pass_prepare_count',
+                                    'pipeline_prepare', start_date,
+                                    finish_date, 'success');
+            buildsRetriever.refresh(binding, 'pass_test_cloud_image_count',
+                                    'test_cloud_image', start_date,
+                                    finish_date, 'success');
+        };
+
+        updateStats('2015-01-01', '2016-01-01');
+
+        function updateDates(value) {
+            switch(value) {
+                case "Last 24 Hours":
+                    console.log("Updating to last 24 hours.");
+                    break;
+                case "Last 7 Days":
+                    console.log("Updating to last 7 days.");
+                    break;
+                case "Last 30 Days":
+                    console.log("Updating to last 30 days.");
+                    break;
+                default:
+                    console.log("Bad date value: " + value);
+            }
+        }
 
         $scope.updateFilter = function(type, value, tab) {
-            console.log("Updating filter!")
+            console.log("Updating filter! %s %s %s", type, value, tab);
+
+            if (type == "date") {
+                updateDates(value);
+            }
         }
     }]);
