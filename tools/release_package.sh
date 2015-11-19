82ca3c2a90a61948e7857903d14a3c2da441254e
Jason Hobbs <jason.hobbs@canonical.com>
2015-11-19 14:07:37 -0600

    Use git describe for version.

--- tools/release_package.sh
+++ tools/release_package.sh
@@ -3,12 +3,10 @@
 rm -rf *.egg-info
 [[ -n "$(git status --porcelain)" ]] && echo "Repo not clean" && exit 1
 distro=${1:-"precise"}
-version="$(python -c 'from weeblclient import weebl_python2; print(weebl_python2.__version__)')"
-echo $version
-git_commit=$(git log -1 --date=short --format="%h")
-dch -b -D $distro \
-  --newversion ${version}~git${git_commit}~${distro}-0ubuntu1 \
-  "PPA build."
+git_version=$(git describe --dirty)
+full_version=${git_version}~${distro}-0ubuntu1
+echo $full_version
+dch -b -D $distro --newversion ${full_version} "PPA build."
 debcommit
 git clean -xdf
 fakeroot debian/rules get-orig-source
@@ -18,4 +16,4 @@ rc=$?
 git reset --hard HEAD
 rm -rf *.egg-info
 [[ ! $rc ]] && echo "Build failed" && exit 1
-echo "Run: dput ppa:canonical-ci/oil-ci ../weeblclient*git${git_commit}*.changes"
+echo "Run: dput ppa:canonical-ci/oil-ci ../weeblclient_${full_version}_source.changes"
