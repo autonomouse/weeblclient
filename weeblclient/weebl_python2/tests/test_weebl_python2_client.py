51c67a81a7688c49301b2e8eea52b37b36257671
Greg Lutostanski <gregory.lutostanski@canonical.com>
2016-07-18 17:35:50 -0500

    fix testcases for new bug_info

--- weeblclient/weebl_python2/tests/test_weebl_python2_client.py
+++ weeblclient/weebl_python2/tests/test_weebl_python2_client.py
@@ -7,83 +7,52 @@ class WeeblClientTests(testtools.TestCase):
 
 
 class WeeblClientUtilsTests(testtools.TestCase):
-    def test_build_dict_of_linked_items(self):
-        file1 = utils.generate_random_string()
-        file2 = utils.generate_random_string()
-        unused_file = utils.generate_random_string()
-        target_file_globs = [
-            {'glob_pattern': file1,
-             'jobtypes': [{'name': 'pipeline_deploy'}],
-             'resource_uri': {'glob_pattern': file1}},
-            {'glob_pattern': file2,
-             'jobtypes': [{'name': 'pipeline_deploy'}],
-             'resource_uri': {'glob_pattern': file2}},
-            {'glob_pattern': unused_file,
-             'jobtypes': [{'name': 'test_tempest_smoke'}],
-             'resource_uri': {'glob_pattern': unused_file}}]
-        known_bug_regexes = [
-            {'bug': '/api/v1/bug/bug1/',
-             'regex': 're_1',
-             'resource_uri': '/api/v1/knownbugregex/kbr1/',
-             'targetfileglobs': [{'glob_pattern': file1}],
-             'uuid': 'kbr1'},
-            {'bug': '/api/v1/bug/bug2/',
-             'regex': 're_2',
-             'resource_uri': '/api/v1/knownbugregex/kbr2/',
-             'targetfileglobs': [{'glob_pattern': file2}],
-             'uuid': 'kbr2'}, ]
-        bugs = [
-            {'bugtrackerbug': {'bug_number': 1,
-                               'resource_uri': '/api/v1/bugtrackerbug/lpbug1/',
-                               'uuid': 'lpbug1'},
-             'knownbugregex': ['/api/v1/knownbugregex/kbr1/'],
-             'resource_uri': '/api/v1/bug/bug1/',
-             'uuid': 'bug1'},
-            {'bugtrackerbug': {'bug_number': 2,
-                               'resource_uri': '/api/v1/bugtrackerbug/lpbug2/',
-                               'uuid': 'lpbug2'},
-             'knownbugregex': ['/api/v1/knownbugregex/kbr2/'],
-             'resource_uri': '/api/v1/bug/bug2/',
-             'uuid': 'bug2'}, ]
-
-        regex_dict, tfile_list = utils.build_dict_of_linked_items(
-            target_file_globs, known_bug_regexes, bugs)
-
-        ideal_regex_dict = {
-            're_1': [(file1, 'pipeline_deploy', 1, 'kbr1')],
-            're_2': [(file2, 'pipeline_deploy', 2, 'kbr2')]}
-        ideal_tfile_list = set([file1, file2])
-        self.assertEqual(ideal_regex_dict, regex_dict)
-        self.assertEqual(ideal_tfile_list, tfile_list)
-        self.assertNotIn(unused_file, tfile_list)
-
     def test_build_bug_info_dict(self):
-        regex_dict = {'re_1': [('tempest_xunit.xml', 'test_tempest_smoke', 1,
-                                'kbr1')],
-                      're_2': [('console.txt', 'pipeline_deploy', 2, 'kbr2'),
-                               ('juju_status.yaml', 'pipeline_prepare', 2,
-                                'kbr2'),
-                               ('juju_status.yaml', 'pipeline_prepare', 3,
-                                'kbr2'), ]}
-        tfile_list = ['juju_status.yaml', 'console.txt', 'tempest_xunit.xml', ]
-        wbugs = [{'bugtrackerbug': {'bug_number': 1}},
-                 {'bugtrackerbug': {'bug_number': 2}},
-                 {'bugtrackerbug': {'bug_number': 3}}, ]
-        output = utils.build_bug_info_dict(regex_dict, tfile_list, wbugs)
+        regexes = [
+         {u'bug': {u'bugtrackerbug': {u'bug_number': 905638,
+            u'created_at': u'2016-07-05T18:45:00.800467',
+            u'project': {u'name': u'qdaemon',
+             u'uuid': u'57e49478-d678-4230-b9e9-7cba1418837f'},
+            u'updated_at': u'2016-07-05T18:45:00.800467',
+            u'uuid': u'8daca2b5-55d3-494e-9ec1-b4c19479b139'},
+           u'description': None,
+           u'last_seen': u'2016-05-31T12:01:06',
+           u'occurrence_count': 40,
+           u'summary': u'qdaemon failed to restart charm',
+           u'uuid': u'21755bbc-2874-47f0-aa4e-5c1223059d64'},
+          u'regex': u'45886{93802}-62702(52772)',
+          u'targetfileglobs': [{u'glob_pattern': u'console.txt',
+          u'jobtypes': [{u'description': None, u'name': u'pipeline_start'}]}],
+          u'uuid': u'63fedc7e-ca8f-4a12-9de9-3fd81ecd2e72'},
+         {u'bug': {u'bugtrackerbug': {u'bug_number': 1578891,
+            u'created_at': u'2016-07-05T18:45:01.179299',
+            u'project': {u'name': u'kernel',
+             u'uuid': u'80bee406-3bec-4465-86f2-e6c8ce6b091a'},
+            u'updated_at': u'2016-07-05T18:45:01.179299',
+            u'uuid': u'3c1e96b8-549c-478e-8067-41431ed1058b'},
+           u'description': None,
+           u'last_seen': u'2016-07-03T16:18:19',
+           u'occurrence_count': 39,
+           u'summary': u'kernel did not start up',
+           u'uuid': u'7ce19b08-9876-46b8-8028-c7d75cd6b938'},
+          u'regex': u'13539{94476}-97589(5091)',
+          u'targetfileglobs': [{u'glob_pattern': u'juju_debug_log.txt',
+          u'jobtypes': [{u'description': None, u'name': u'pipeline_deploy'}]}],
+          u'uuid': u'bb14eb84-4708-43ad-a8dc-81137aded083'}]
+
+        output = utils.munge_bug_info_data(regexes)
 
         ideal_output = {'bugs': {
-            1: {'regex_uuid': 'kbr1',
-                'test_tempest_smoke': [
-                    {'tempest_xunit.xml': {'regexp': ['re_1']}}]},
-            2: {'regex_uuid': 'kbr2',
-                'pipeline_deploy': [
-                    {'console.txt': {'regexp': ['re_2']}}],
-                'pipeline_prepare': [
-                    {'juju_status.yaml': {'regexp': ['re_2']}}]},
-            3: {'regex_uuid': 'kbr2',
-                'pipeline_prepare': [
-                    {'juju_status.yaml': {'regexp': ['re_2']}}]}
-            }}
+            905638: {
+                u'pipeline_start':
+                    {'regexp': [u'45886{93802}-62702(52772)'],
+                     'uuids': [u'63fedc7e-ca8f-4a12-9de9-3fd81ecd2e72']},
+                'regex_uuid': u'63fedc7e-ca8f-4a12-9de9-3fd81ecd2e72'},
+            1578891: {
+                u'pipeline_deploy':
+                    {'regexp': [u'13539{94476}-97589(5091)'],
+                     'uuids': [u'bb14eb84-4708-43ad-a8dc-81137aded083']},
+                'regex_uuid': u'bb14eb84-4708-43ad-a8dc-81137aded083'}}}
 
         self.assertEqual(ideal_output, output)
 
